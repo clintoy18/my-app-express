0d539144da710a7a4e1b4e754e4f371f60b5a3e0
unknown <TT402@ccs.local>
2025-02-05 17:41:51 +0800

    Serving a static file

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
+//serve static files from the "public" folder
+app.use(express.static('public'));
+
 //Define the route for the home page
 app.get('/', (req,res) => {   
     res.send("Hello World");
